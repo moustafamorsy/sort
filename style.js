f79818b2886153bdfdf9ff66d6e86e558be5fdca
moustafamorsy <mostafamorsyyy@gmail.com>
2022-07-11 08:34:08 +0200

    sort

---
+++ style.js
@@ -0,0 +1,27 @@
+
+let unsorted = [5 , 7 , 1 , 3 , 10 , 8 , 9 , 4 , 2 , 6 ]
+
+function sort(array) {
+
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array.length; j++) {
+        if (array[j] < array[j-1]) {
+          swap(array , j , j-1);}
+    }  
+   
+    }
+    console.log(array);
+}
+
+function swap(array, j , change) {
+
+ let temp = array[j] ;
+
+ array[j] = array[change] ;
+
+ array[change] = temp ;
+
+}
+
+
+console.log(sort(unsorted));
