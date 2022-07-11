141c60a6cdb35881de1e2a285d751530f077f045
moustafamorsy <mostafamorsyyy@gmail.com>
2022-07-11 08:49:10 +0200

    sorting

--- style.js
+++ style.js
@@ -1,12 +1,13 @@
 
-let unsorted = [5 , 7 , 1 , 3 , 10 , 8 , 9 , 4 , 2 , 6 ]
+let unsorted = [5 , 50 , 7 , 1 , 3 , 10 , 8 , 9 , 4 , 2 , 6  ]
 
 function sort(array) {
 
     for (let i = 0; i < array.length; i++) {
         for (let j = 0; j < array.length; j++) {
-        if (array[j] < array[j-1]) {
-          swap(array , j , j-1);}
+        if (array[j] > array[j+1]) {
+          swap(array , j , j+1);}
+          console.log(array)
     }  
    
     }
